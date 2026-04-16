2cc8877ed4d85762a8f5730376d3f65d3f268ae9
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-04-16 17:00:48 +0300

    making step 6 from 23

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import requests
 
-url = 'https://dvmn.org/media/HST-SM4.jpeg'
+#url = 'https://dvmn.org/media/HST-SM4.jpeg'
 spacex_api_url = 'https://api.spacexdata.com/v5/launches/61fc0243e0dc5662b76489ae'
 path = 'images'
 
@@ -11,22 +11,29 @@ proxies = {
     'https': 'socks5://89.169.168.25:1080',
     }
 
-
-def get_picture(url, path):
+def get_picture(url, path, index):
     os.makedirs(path, exist_ok=True)
 
-    response = requests.get(url, proxies=proxies)
+    #response = requests.get(url, proxies=proxies)
+    response = requests.get(url)
     response.raise_for_status()
-
-    with open(f'{path}/hubble.jpeg', 'wb') as file:
+    
+    with open(f'{path}/spacex_{index}.jpeg', 'wb') as file:
         file.write(response.content)
+        
 
 
 def get_pucture_from_spacex(url):
     response = requests.get(url)
     response.raise_for_status()
 
-    print(response.json()['links']['flickr']['original'])
+    url_photos_json = response.json()['links']['flickr']['original']
+    print(url_photos_json)    
+    
+    for index, url in enumerate(url_photos_json):
+        
+        get_picture(url, path, index)
+       
 
 
 if __name__ == '__main__':
