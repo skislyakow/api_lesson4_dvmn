21cd0fb1e6d718d07625fa6a79acb4de333e7213
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-04-20 16:43:45 +0300

    proccessing step8 from 23

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 import requests
 
+from dotenv import load_dotenv
+
 #url = 'https://dvmn.org/media/HST-SM4.jpeg'
 spacex_api_url = 'https://api.spacexdata.com/v5/launches/61fc0243e0dc5662b76489ae'
 path = 'images'
@@ -33,12 +35,23 @@ def fetch_spacex_last_launch(url):
     for index, url in enumerate(url_photos_json):
         
         get_picture(url, path, index)
+
+
+def fetch_apod():
+    apod_url = 'https://api.nasa.gov/planetary/apod?api_key=API_KEY'
+    response = requests.get(apod_url, proxies=proxies)
+    response.raise_for_status()
+
+    with open(f'{path}/apod.jpeg', 'wb') as file:
+        file.write(response.content)
        
 
 
 if __name__ == '__main__':
+    load_dotenv()
     #get_picture(url, path)
 
     #print(requests.get('https://api.ipify.org', proxies=proxies).text)
 
-    fetch_spacex_last_launch(spacex_api_url)
+    #fetch_spacex_last_launch(spacex_api_url)
+    fetch_apod()
