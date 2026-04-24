b69905ce5139c7371bb9c6edb65ebc49bbcec880
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-04-24 14:18:43 +0300

    add DEMO_KEY for alternative use

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 from os.path import split, splitext
-from urllib.parse import urlsplit, unquote
+from urllib.parse import urlsplit, unquote, urlencode
 
 import requests
 
 from environs import Env
 
-#url = 'https://dvmn.org/media/HST-SM4.jpeg'
 
+DEMO_KEY = 'DEMO_KEY'
+PATH = 'images'
 
 
 def get_picture(url, path, index):
@@ -17,7 +18,6 @@ def get_picture(url, path, index):
     
     with open(f'{path}/spacex_{index}{get_file_extension(url)}', 'wb') as file:
         file.write(response.content)
-        
 
 
 def fetch_spacex_last_launch(url):
@@ -29,25 +29,40 @@ def fetch_spacex_last_launch(url):
     
     for index, url in enumerate(url_photos_json):
         
-        get_picture(url, path, index)
+        get_picture(url, PATH, index)
 
 
-def fetch_apod():
+def fetch_apod(base_url, path, primary_key):
     os.makedirs(path, exist_ok=True)
+
+    def make_request(api_key):
+        params = {
+            'apikey': api_key,
+            'count': 10,
+        }
+        url = f'{base_url}?{urlencode(params)}'
+        return requests.get(url)
     try:
-        response = requests.get(apod_url)
+        response = make_request(primary_key)
         response.raise_for_status()
-        apod_list = response.json()
-        for index, apod in enumerate(apod_list):
-            url_photo = apod['hdurl']
-            extension = get_file_extension(url_photo)
-            photo = requests.get(url_photo)
-            with open(f'{path}/apod_{index}{extension}', 'wb') as file:
-                file.write(photo.content) 
     except requests.exceptions.HTTPError as error:
-        print(f'Ошибка HTTP: {error}')
-    except requests.exceptions.ConnectionError as error:
-        print(f'Ошибка соединения: {error}')
+        if error.response.status_code == 403:
+            print(f'{error.response.status_code}, далее использовать DEMO_KEY')
+            try:
+                response = make_request(DEMO_KEY)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as error2:
+                print(f'Основной ключ, ошибка: {error.response.status_code}')
+                print(f'DEMO_KEY, ошибка: {error2.response.status_code}')
+                return
+            
+    apod_list = response.json()
+    for index, apod in enumerate(apod_list):
+        url_photo = apod['hdurl']
+        extension = get_file_extension(url_photo)
+        photo = requests.get(url_photo)
+        with open(f'{path}/apod_{index}{extension}', 'wb') as file:
+            file.write(photo.content) 
 
 
 def get_file_extension(url: str) -> str:
@@ -56,25 +71,17 @@ def get_file_extension(url: str) -> str:
     _, filename = split(decoded_url)
     _, extension = splitext(filename)
     return extension
-       
 
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
     api_key = env.str("NASA_DEMO_API")
-    apod_url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&count=30'
-    spacex_api_url = 'https://api.spacexdata.com/v5/launches/61fc0243e0dc5662b76489ae'
-    path = 'images'
+    base_apod_url = 'https://api.nasa.gov/planetary/apod'
+    spacex_api_url = 'https://api.spacexdata.com/v5/launches/61fc0243e0dc5662b76489ae' 
 
     proxies = {
         'http': 'socks5://89.169.168.25:1080',
         'https': 'socks5://89.169.168.25:1080',
         }
-    #load_dotenv()
-    #get_picture(url, path)
-
-    #print(requests.get('https://api.ipify.org', proxies=proxies).text)
-
-    #fetch_spacex_last_launch(spacex_api_url)
-    fetch_apod()
+    fetch_apod(base_apod_url, PATH, api_key)
